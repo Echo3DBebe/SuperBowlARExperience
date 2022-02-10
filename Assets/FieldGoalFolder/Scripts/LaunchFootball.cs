452e32a3a4130c3744c65a52f534ad124c95c8b5
Bebe <bebethegirl@gmail.com>
2022-02-09 20:30:30 -0800

    Added projectile script and objects

--- Assets/FieldGoalFolder/Scripts/LaunchFootball.cs
+++ Assets/FieldGoalFolder/Scripts/LaunchFootball.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class LaunchFootball : MonoBehaviour
 {
+    [SerializeField]
+    private Transform footballSpawn;
 
-   
+    [SerializeField]
+    private Rigidbody football;
+
+    [SerializeField]
+    private float footballSpeed;
+
+    private Touch tapToThrow;
+
+    private void Start()
+    {
+        footballSpeed = 300f;
+    }
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.touchCount > 0)
+        {
+            var newFootball = Instantiate(football, footballSpawn.position, footballSpawn.rotation);
+            newFootball.AddForce(footballSpawn.forward * footballSpeed);
+
+            print("Touchdown!");
+        }
     }
 }
