4f6d5ceab5e32910f71cbe3a6f6a57c73a06887c
Bebe <bebethegirl@gmail.com>
2022-02-11 16:02:39 -0800

    Full loop works

--- Assets/FieldGoalFolder/Scripts/FieldGoalTrigger.cs
+++ Assets/FieldGoalFolder/Scripts/FieldGoalTrigger.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldGoalTrigger : MonoBehaviour
-{   
+{
+    // [SerializeField]
+    // private Transform footballSpawn;
+
+    [SerializeField]
+    private Rigidbody football;
+
+    [SerializeField]
+    private float footballSpeed;
+
     [SerializeField]
     public AudioSource cheeringSource;
+
+    private Touch tapToThrow;
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
+
+
+    void Start()
+    {
+        startingPosition = football.transform.position;
+        startingRotation = football.transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.touchCount > 0)
+        {
+            // Instantiates a new football when input is given
+            // var newFootball = Instantiate(football, footballSpawn.position, footballSpawn.rotation);
+
+            football.AddForce(-football.transform.right * footballSpeed, ForceMode.VelocityChange);
+            print("Shooting the shot");
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         cheeringSource.Play();
         print("Touchdown!");
+
+        football.transform.position = startingPosition;
+        football.transform.rotation = startingRotation;
+
+        //  football.transform.position = footballSpawn.position;
+        football.velocity = Vector3.zero;
     }
 }
